aec759640be793952bc35a48a8302561c50924f5
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-20 12:13:36 +0400

    add dump dir symlink

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mkdir -p /var/www/nailgun/dump
+#link dump dir
+rm -rf /var/www/nailgun/dump
+ln -s /dump /var/www/nailgun/
 chmod -R 755 /var/www/nailgun
 puppet apply -v /etc/puppet/modules/nailgun/examples/nginx-only.pp
 nginx -g 'daemon off;'
